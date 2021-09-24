5423ddeaac91812853f169c63eb63152246fcc9c
Dan Halbert <halbert@halwitz.org>
2021-09-24 16:30:54 -0400

    Add empty System Control Collection to make visible in /dev/input on Linux.
    The Surface Dial shows up in /dev/input on Linux, though it doesn't have much functionality.
    However, the previous report descriptor did not cause our radial controller to show up.
    By process of elimination, I discovered that a System Control Collection added to the descriptor
    causes it to show up. This makes the device potentially easier to use on Linux, by tracking
    events directly, instead of having to use hidraw. I have not yet found anything in the Linux
    kernel HID drivers that would explain why this works.

--- adafruit_radial_controller/device.py
+++ adafruit_radial_controller/device.py
@@ -63,6 +63,12 @@ def device(report_id):
 
             0xc0,             #   End Collection
             0xc0,             # End Collection
+
+            # The presence of this (even empty) System Control Collection makes the device
+            # appear in /dev/input in Linux. Don't know why this works yet.
+            0x09, 0x80,       # Usage (System Control)
+            0xa1, 0x01,       # Collection (Application)
+            0xc0,             # End Collection
             )),
         # fmt: on
         usage_page=0x01,
